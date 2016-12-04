83194318e6f59560fcb305e6d6ca5257b335d2e7
Debora Wöpcke <deboradub@gmail.com>
2016-12-04 00:22:32 +0000

    Add a scenario with a second participant.

--- features/participant.feature
+++ features/participant.feature
@@ -3,4 +3,11 @@ Feature: Testing the Participants class only.
   Scenario: A first participant is added.
     Given there are no participants
      when a participant is added
-     then a block is found.
+     then a block is found
+      and there is one participant
+
+  Scenario: A second participant with the same block.
+    Given one participant with a block
+     when another participant is added
+     then a new block is not found
+      and there are two participants
--- features/steps/participants.py
+++ features/steps/participants.py
@@ -1,12 +1,13 @@
 import os
 
-from bc import Participants, CACHE_DIR, toFilename
+from bc import Participants, CACHE_DIR, toFilename, toParticipant
 
 
 class FCPNode(object):
     def _submitCmd(self, *args, **kwargs):
         kwargs["callback"]("successful",
-                           {"header":"FoundURI", "URI":"FOUNDURI"})
+                           {"header":"Found" + kwargs["URI"],
+                            "URI":"FOUNDURI" + kwargs["URI"]})
 
 
 class Node(object):
@@ -31,6 +32,28 @@ def step_impl(context):
 def step_impl(context):
     context.tested_object.add_participant("URI")
 
-@then(u'a block is found.')
+@then(u'a block is found')
 def step_impl(context):
     assert context.tested_object.get_new_block_reference()
+
+@given(u'one participant with a block')
+def step_impl(context):
+    context.tested_object = Participants(Node(), Fetcher())
+    context.tested_object.add_participant("URI")
+    assert context.tested_object.get_new_block_reference()
+
+@when(u'another participant is added')
+def step_impl(context):
+    context.tested_object.add_participant("URI2")
+
+@then(u'a new block is not found')
+def step_impl(context):
+    assert not context.tested_object.get_new_block_reference()
+
+@then(u'there is one participant')
+def step_impl(context):
+    assert 1 == len(list(context.tested_object.get_participants()))
+
+@then(u'there are two participants')
+def step_impl(context):
+    assert 2 == len(list(context.tested_object.get_participants()))
